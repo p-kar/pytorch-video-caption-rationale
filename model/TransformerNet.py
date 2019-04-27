962910c53d341c2ed9c85839a1930187103741ba
Aastha Tripathi <madhumithasakthi.iyer@utexas.edu>
2019-04-27 15:18:25 -0500

    fixed transformer

--- model/TransformerNet.py
+++ model/TransformerNet.py
@@ -19,14 +19,19 @@ class MultiHeadAttention(nn.Module):
         self.d_k = hidden_size // heads
         self.h = heads
         if flag == 'e':
-           inp_size = 4096
+           inp_size1 = inp_size2 = inp_size3 = 4096
+          
+        elif flag == 'd':
+           inp_size1 = inp_size2 = inp_size3 = 300
         else:
-           inp_size = 300
-        self.q_linear = nn.Linear(inp_size, hidden_size)
-        self.v_linear = nn.Linear(inp_size, hidden_size)
-        self.k_linear = nn.Linear(inp_size, hidden_size)
+           inp_size1 = 300
+           inp_size2 = 4096
+           inp_size3 = 4096
+        self.q_linear = nn.Linear(inp_size1, hidden_size)
+        self.v_linear = nn.Linear(inp_size2, hidden_size)
+        self.k_linear = nn.Linear(inp_size3, hidden_size)
         self.dropout = nn.Dropout(dropout)
-        self.out = nn.Linear(hidden_size, inp_size)
+        self.out = nn.Linear(hidden_size, inp_size1)
     
     def forward(self, q, k, v, mask=None):
         
@@ -42,7 +47,7 @@ class MultiHeadAttention(nn.Module):
         k = k.transpose(1,2)
         q = q.transpose(1,2)
         v = v.transpose(1,2)
-        print ("k size:", k.shape)
+        #print ("k size:", k.shape)
 # calculate attention using function we will define next
         scores = attention(q, k, v, self.d_k, mask, self.dropout)
         
@@ -57,10 +62,10 @@ class MultiHeadAttention(nn.Module):
 def attention(q, k, v, d_k, mask=None, dropout=None):
     
     scores = torch.matmul(q, k.transpose(-2, -1)) /  math.sqrt(d_k)
-    print ("scores shape:", scores.shape)
+    #print ("scores shape:", scores.shape)
     if mask is not None:
         mask = mask.unsqueeze(1)
-        print ("mask shape after unsqueeze:", mask.shape)
+        #print ("mask shape after unsqueeze:", mask.shape)
         scores = scores.masked_fill(mask == 0, -1e9)
     scores = F.softmax(scores, dim=-1)
     
@@ -91,8 +96,10 @@ class Norm(nn.Module):
         super().__init__()
         if flag == 'e':
            self.size = 4096#hidden_size
-        else:
+        elif flag == 'd':
            self.size = 300
+        else:
+           self.size = 7893 
         # create two learnable parameters to calibrate normalisation
         self.alpha = nn.Parameter(torch.ones(self.size))
         self.bias = nn.Parameter(torch.zeros(self.size))
@@ -164,14 +171,15 @@ class DecoderLayer(nn.Module):
         self.dropout_3 = nn.Dropout(dropout)
         
         self.attn_1 = MultiHeadAttention(heads, hidden_size,flag)
-        self.attn_2 = MultiHeadAttention(heads, hidden_size,flag)
+        self.attn_2 = MultiHeadAttention(heads, hidden_size,'ed')
         self.ff = FeedForward(hidden_size,flag).cuda()
     def forward(self, x, e_outputs, src_mask, trg_mask):
         x2 = self.norm_1(x)
-        print ("x2 shape:", x2.shape)
-        print ("trg_mask shape:",trg_mask.shape)
+        #print ("x2 shape:", x2.shape)
+        #print ("trg_mask shape:",trg_mask.shape)
         x = x + self.dropout_1(self.attn_1(x2, x2, x2, trg_mask))
         x2 = self.norm_2(x)
+        #print ("e_outputs shape:", e_outputs.shape)
         x = x + self.dropout_2(self.attn_2(x2, e_outputs, e_outputs,src_mask))
         x2 = self.norm_3(x)
         x = x + self.dropout_3(self.ff(x2))
@@ -219,7 +227,8 @@ class Decoder(nn.Module):
         #self.pe = PositionalEncoder(hidden_size)
         self.layers = get_clones(DecoderLayer(hidden_size, heads,flag), N)
         self.norm = Norm(hidden_size,flag)
-        self.pred_linear = nn.Sequential(nn.Dropout(p=dropout_p), nn.Linear(hidden_size, self.vocab_size))
+        self.norm_out = Norm(hidden_size,'no')
+        self.pred_linear = nn.Sequential(nn.Dropout(p=dropout_p), nn.Linear(300, self.vocab_size))
 
     def forward(self, e_outputs, s, src_mask, trg_mask):
         if self.training:
@@ -230,44 +239,54 @@ class Decoder(nn.Module):
         num_frames = e_outputs.shape[1]
         sos_tags = torch.ones((batch_size, 1), dtype=torch.long) * self.sos_id
         sos_tags = sos_tags.to(device)
-        if self.training:
-            s = torch.cat((sos_tags, s), dim=1)
-            # right shifted sentence - (B x (L + 1))
-        else:
-            s = sos_tags
-        curr_words = s[:,0]
-        #x = self.embed(curr_words)
-        #x = self.pe(x)
+        if not self.training:
+           s = sos_tags
+        curr_words = s
         logits = None
-        print ("self.max_len:", self.max_len)
-        for i in range(self.max_len):
+        curr_words = self.embedding(curr_words)
+        print ("curr_words shape:",curr_words.shape )
+        #for i in range(self.max_len):
+        if self.training:
             for j in range(self.N):
-                curr_words = self.embedding(curr_words)
-                print ("curr_words shape:",curr_words.shape )
-                x = self.layers[j](curr_words, e_outputs, src_mask, trg_mask[:,:,i].unsqueeze(2))
+                x = self.layers[j](curr_words, e_outputs, src_mask, trg_mask)#.unsqueeze(2))
             x = self.pred_linear(x.squeeze(0))
-            outs = self.norm(x)
-            if logits is None:
-                logits = outs.unsqueeze(0)
-            else:
-                logits = torch.cat((logits, outs.unsqueeze(0)), dim=0)
-            if self.training:
-                curr_words = s[:, i + 1]
-            else:
-                curr_words = torch.argmax(outs, dim=1)
-
-        logits = torch.transpose(logits, 0, 1).contiguous()
+            outs = self.norm_out(x)
+            logits = outs
         # B x L x vocab_size
-
+        else:
+            #curr_words = s[:, 0]
+            logits = None
+            outputs = torch.zeros(e_outputs.shape[0],self.max_len).type_as(s)
+            #print ("outputs shape:", outputs.shape)
+            #print ("sos_tags shape:",sos_tags.shape)
+            for x in range(outputs.shape[0]):
+                outputs[x,0] = sos_tags[x]
+            #print (outputs)
+            outputs = self.embedding(outputs)
+            for i in range(1, self.max_len+1):
+                  print ("max_len i:", i)
+                  trg_mask = np.triu(np.ones((1, i, i)),k=1).astype('uint8')
+                  trg_mask = Variable(torch.from_numpy(trg_mask) == 0).cuda()
+                  for j in range(self.N):
+                      x = self.layers[j](outputs[:,:i], e_outputs, src_mask, trg_mask)#.unsqueeze(2))
+                  x = self.pred_linear(x.squeeze(0))
+                  outs = self.norm_out(x)
+                  print ("outs shape:",outs.shape)
+                  #if logits is None:
+                  #   logits = outs.unsqueeze(0)
+                  #else:
+                  #   logits = torch.cat((logits, outs.unsqueeze(0)), dim=0)
+                  #curr_words = torch.argmax(outs, dim=1)
+            #logits = torch.transpose(logits, 0, 1).contiguous()
+            logits = outs
         return logits
 
 def create_masks_inp(vid_features):
         input_msk = torch.ones(vid_features.shape[0],vid_features.shape[1]).cuda()
         input_msk = input_msk.unsqueeze(1)
-        print ("input_msk.length:", input_msk.shape)
         return input_msk
 
-def create_masks_trg(self,s=None, s_len= None):
+def create_masks_trg(self, bsize, s=None, s_len= None):
        if self.training:
           trg_mask = torch.zeros(s.shape[0],s.shape[1]).cuda()
           for x in range(s.shape[0]):
@@ -281,11 +300,11 @@ def create_masks_trg(self,s=None, s_len= None):
           nopeak_mask = np.triu(np.ones((1, size, size)),k=1).astype('uint8')
           #nopeak_mask = nopeak_mask.astype('uint8')
           nopeak_mask = Variable(torch.from_numpy(nopeak_mask) == 0).cuda()
-          print("trg_mask shape before nopeak:", trg_mask.shape)
+          #print("trg_mask shape before nopeak:", trg_mask.shape)
           trg_mask = trg_mask & nopeak_mask
-          print("trg_mask shape:", trg_mask.shape)
        else:
-          trg_mask = np.triu(np.ones((1, i, i)),k=1).astype('uint8')
+          
+          trg_mask = np.triu(np.ones((bsize, 1, 1)),k=1).astype('uint8')
           trg_mask= Variable(torch.from_numpy(trg_mask) == 0).cuda()
        return trg_mask 
 
@@ -299,9 +318,14 @@ class Transformer(nn.Module):
         self.decoder = Decoder(glove_loader, hidden_size, dropout_p, max_len, N, heads,flag ='d')
         #self.out = nn.Linear(hidden_size, glove_loader)
     def forward(self, vid_features, s=None, s_len=None):# __trg__, __src_mask__, __trg_mask__):
+        #print ("s:", s)
+        print ("self.training:", self.training)
         src_mask = create_masks_inp(vid_features)
+        print ("src_mask shape:", src_mask.shape)
         e_outputs = self.encoder(vid_features,src_mask)
-        trg_mask = create_masks_trg(self,s,s_len)
+        trg_mask = create_masks_trg(self,vid_features.shape[0], s, s_len)
+        print ("trg_mask shape:", trg_mask.shape)
         logits = self.decoder(e_outputs,s,src_mask,trg_mask)
+        print ("logits shape:", logits.shape)
         #logits = self.out(d_output)
         return logits
